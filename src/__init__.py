961e6f9c00973dba084ea23367186821412dd457
flenbob <ivan.flensburg@gmail.com>
2023-09-08 19:56:26 +0200

    create package

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .packing import Particles, Param, CoordinatesGenerator, CollectionIntervalGenerator, ParticlesGenerator, Packing
+#from .datawriter import DataWriter
+#from .filename import filename
+from .runner import LAMMPSScript, Runner
+#from .lmpm import LinearMixturePackingModel
