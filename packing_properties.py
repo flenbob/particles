47f411ba46df366be2f7c70482d5b7a6614b58a0
flenbob <ivan.flensburg@gmail.com>
2023-11-06 00:50:24 +0100

    remove file

--- packing_properties.py
+++
@@ -1,8 +0,0 @@
-#TODO: Infer packing properties:
-#   -- Input --:
-#   - CSV-table or packing file
-
-#   -- Output --:
-#   - Conent uniformity using Stange and Hilden measured in COV of concentration, volume, mass
-#   - Predicted packing fraction by LMPM
-#   - Polydispersity
